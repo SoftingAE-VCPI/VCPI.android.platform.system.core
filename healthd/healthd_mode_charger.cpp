f53683debfe676545518a74070038243513abfe3
SagarMakhar <sagar.m@l4b-software.com>
2024-03-25 08:07:18 +0000

    Fix charging animation and device booting at incorrect percentage

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -501,7 +501,7 @@ void Charger::ProcessKey(int code, int64_t now) {
                 } else {
                     if (batt_anim_.cur_level >= boot_min_cap_) {
                         LOGW("[%" PRId64 "] rebooting\n", now);
-                        reboot(RB_AUTOBOOT);
+                        //reboot(RB_AUTOBOOT);
                     } else {
                         LOGV("[%" PRId64
                              "] ignore power-button press, battery level "
@@ -579,7 +579,7 @@ void Charger::HandlePowerSupplyState(int64_t now) {
                  now, (int64_t)timer_shutdown, next_pwr_check_);
         } else if (now >= next_pwr_check_) {
             LOGW("[%" PRId64 "] shutting down\n", now);
-            reboot(RB_POWER_OFF);
+            //reboot(RB_POWER_OFF);
         } else {
             /* otherwise we already have a shutdown timer scheduled */
         }
@@ -611,6 +611,10 @@ void Charger::OnHeartbeat() {
      * screen transitions (animations, etc)
      */
     UpdateScreenState(now);
+    if (health_info_.battery_level >= MIN_BATTERY_FOR_BOOT) {
+        LOGW("rebooting\n");
+        reboot(RB_AUTOBOOT);
+    }
 }
 
 void Charger::OnHealthInfoChanged(const ChargerHealthInfo& health_info) {
@@ -622,11 +626,6 @@ void Charger::OnHealthInfoChanged(const ChargerHealthInfo& health_info) {
         kick_animation(&batt_anim_);
     }
     health_info_ = health_info;
-
-    if (health_info_.battery_level >= MIN_BATTERY_FOR_BOOT) {
-        LOGW("rebooting\n");
-        reboot(RB_AUTOBOOT);
-    }
 }
 
 int Charger::OnPrepareToWait(void) {
